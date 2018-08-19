cea22a4685d2c7155336ff280ed33f8a8a7711a2
binay2012 <31483288+binay2012@users.noreply.github.com>
2018-08-19 13:43:34 +0300

    final

--- OsamantiShelf/app/__init__.py
+++ OsamantiShelf/app/__init__.py
@@ -6,7 +6,7 @@ from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql://binay2012:Nicosia2018!!@recommendersystems.cam2arfnsa7i.us-east-2.rds.amazonaws.com:5432/RSSystems'
+app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql://binay2012:binay2012@rssystem1.cam2arfnsa7i.us-east-2.rds.amazonaws.com:5432/RSSystems'
 app.config['SECRET_KEY'] = 'secret'
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
